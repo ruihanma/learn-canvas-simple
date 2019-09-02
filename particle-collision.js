094c4e8604b024354ac0f1025c4778f611771947
maruihan <phenix_infinity@hotmail.com>
2019-09-02 14:52:23 +0800

    基础结构

--- particle-collision.js
+++ particle-collision.js
@@ -5,27 +5,50 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 // 对象
-Particle = (x, y, radius, color) => {
-    this.x = x;
-    this.y = y;
-    this.radius = radius;
-    this.color = color;
+function Particle(x, y, radius, color) {
+  this.x = x;
+  this.y = y;
+  this.radius = radius;
+  this.color = color;
 
+  this.update = () => {
+    this.draw();
+  };
+
+  this.draw = () => {
     c.beginPath();
 
     c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    // c.fillStyle = this.color;
+    // c.fill();
+    c.strokeStyle = this.color;
+    c.stroke();
 
     c.closePath();
-} 
+  };
+}
+
+let particles = [];
+let count = 5;
 
 init = () => {
+  for (let i = 0; i < count; i++) {
+    let x = Math.random() * canvas.width,
+      y = Math.random() * canvas.height,
+      radius = 200,
+      color = "#333";
+    particles.push(new Particle(x, y, radius, color));
+  }
 
-}
-
+  console.log("particles", particles);
+};
 
 animate = () => {
-    requestAnimationFrame(animate);
-}
-
+  requestAnimationFrame(animate);
+  particles.forEach(particle => {
+    particle.update();
+  });
+};
 
 init();
+animate();
