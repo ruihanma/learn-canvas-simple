514266d286be3ef9e5fdb2f08b73ef47a5cc9768
maruihan <phenix_infinity@hotmail.com>
2019-09-02 15:19:40 +0800

    位置完成

--- particle-collision.js
+++ particle-collision.js
@@ -4,6 +4,17 @@ const c = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+// 数值随机方法
+function randomIntFromRange(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+// 获取两点之间的距离
+function getDistance(x1, y1, x2, y2) {
+  let d = Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2), 2);
+  return d;
+}
+
 // 对象
 function Particle(x, y, radius, color) {
   this.x = x;
@@ -29,18 +40,30 @@ function Particle(x, y, radius, color) {
 }
 
 let particles = [];
-let count = 5;
+let count = 4;
 
 init = () => {
   for (let i = 0; i < count; i++) {
-    let x = Math.random() * canvas.width,
-      y = Math.random() * canvas.height,
-      radius = 200,
+    let radius = 100,
+      x = randomIntFromRange(radius, innerWidth - radius),
+      y = randomIntFromRange(radius, innerHeight - radius),
       color = "#333";
+
+    if (i !== 0) {
+      for (let j = 0; j < particles.length; j++) {
+        if (
+          getDistance(x, y, particles[j].x, particles[j].y) - radius * 2 <
+          0
+        ) {
+          (x = randomIntFromRange(radius, innerWidth - radius)),
+            (y = randomIntFromRange(radius, innerHeight - radius)),
+            (j = -1);
+        }
+      }
+    }
+
     particles.push(new Particle(x, y, radius, color));
   }
-
-  console.log("particles", particles);
 };
 
 animate = () => {
